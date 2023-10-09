1028f97e3c06dbfc9972732361322aafe67baa1c
SilvanaLorens <silvanalorens@gmail.com>
2023-10-08 19:03:17 -0500

    Function Anonima con interfaces

--- src/samples/Main.java
+++ src/samples/Main.java
@@ -1,23 +1,39 @@
 package samples;
 
-public class Main implements Operation {
+public class Main {
 	
 	public Main() {
-		Operation.interfaceStaticMethod();
-		interfaceDefaultMethod();
 		
+		Operation o1 = new Operation() {
+
+			@Override
+			public float operation() {
+				// TODO Auto-generated method stub
+				var valor1 = 3.3f;
+				print("Primer valor: "+ valor1 );
+				
+				var valor2 = 3;
+				print("Segundo valor: "+ valor2 );
+				
+				return valor1 * valor2;
+			}
+			private void print(String value) {
+				System.out.println(value);
+			}
+		
+		
+	};
+	printResultOfThis(o1);
 	}
 
 	public static void main(String[] args) {
 		new Main();
 		System.out.println("Hola");
-
+		
 	}
 
-	@Override
-	public float operation() {
-		// TODO Auto-generated method stub
-		return 0;
+	private void printResultOfThis(Operation operation) {
+		System.out.println(operation.operation());
 	}
 
 }
